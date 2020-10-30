bec1a86d365b3821408b5c29def13a2ec3703d05
Easy AIKTC <70479170+easyaiktc@users.noreply.github.com>
2020-10-30 14:58:21 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,211 @@
+<?php
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Easy AIKTC</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="home.css">
+    <link href="https://fonts.googleapis.com/css2?family=Caveat&display=swap" rel="stylesheet">
+</head>
+<body onload="preloader()">
+  <div class="bounce" id="loading">
+      <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>
+  
+
+<header class="headerpart bg-light">
+        <div class="headerpart  text-center">
+
+          <div class="header" id="topheader">
+            <nav class="navbar navbar-expand-lg navbar-dark">
+              <div class="container p-2">
+          <a class=" navbar-brand text-dark font-weight-bold" href="#"> Easy AIKTC</a> <img src="ea.png" height="50px" width="150px">
+          <button class="navbar-toggler bg-primary" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item ">
+                <a class="nav-link text-dark" href="../../">SignUp <span class="sr-only">(current)</span></a>
+              </li>
+             
+              <li class="nav-item">
+                <a class="nav-link text-dark" href="about-us">Login</a>
+              </li>
+        
+              <li class="nav-item">
+                <a class="nav-link text-dark" href="about-us">About</a>
+              </li>
+        
+             
+            </ul>
+          </div>
+        </nav>
+        
+<section class="a ">
+    <h1 class="text-center">Welcome to</h1>
+    <div class="aa text-center ">
+     <h1>Easy AIKTC</h1>
+    </div>
+</section>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+    <path fill="#ffffff" fill-opacity="1" d="M0,224L48,213.3C96,203,192,181,288,165.3C384,149,480,139,576,160C672,181,768,235,864,229.3C960,224,1056,160,1152,122.7C1248,85,1344,75,1392,69.3L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+  </svg>
+</header>
+
+
+
+<section class="b">
+
+<div class="container ">
+   <div class="row">
+
+     <div class="extradiv col-lg-7 col-md-7 col-7">
+      <h3>A hardly welcome to Our Easy Aiktc,<br>
+    This site is the platform for the Aikticians, where u will find almost all the notes based on Mumbai University like question papers, books, notes etc. <br>
+We also provide many utilites like CGPA convertor, scientific calculator, simple calculator etc.   </h3>
+          </div>
+     
+     
+
+  <div class=" col-lg-5 col-md-5 col-5" >
+       <div>
+           <img class="photo" style="width:150px;height:250px" src="101.png" alt="">
+            </div>
+         
+        
+  </div>   
+  <hr class="mx-auto container" style="width:50%;">
+
+</div><div style="margin-top: 100px; " class="text-center "><button type="button" class="btn   btn-primary">Login </button> 
+  <button  type="button" class="btn btn-success">Signup </button></div>
+
+</section>
+
+   
+        <!-- Footer -->
+        <footer class="cc page-footer text-white font-small blue pt-4 mt-5">
+            
+             <div class="f-border container mt-5 w-50">
+        
+    </div>
+
+
+          <!-- Footer Links -->
+          <div class="container-fluid text-center text-md-left">
+        
+            <!-- Grid row -->
+            <div class="row">
+        
+              <!-- Grid column -->
+              <div class="col-md-6 mt-md-0 mt-3 text-white">
+        
+                <!-- Content -->
+                <h5 class="text-uppercase">Easy AIKTC</h5>
+                <p>We understand the needs of students,By seeing this views we have created this!</p>
+        
+              </div>
+              <!-- Grid column -->
+        
+              <hr class="clearfix w-70 d-md-none pb-3">
+        
+              <!-- Grid column -->
+              <div class="col-md-3 mb-md-0 mb-3">
+        
+                <!-- Links -->
+                <h5 class="text-uppercase"><i class="fa fa-link" aria-hidden="true"></i>
+                  Links</h5>
+        
+                <ul class="list-unstyled">
+                  <li>
+                    <a href="https://www.aiktc.ac.in/?fbclid=IwAR3SVEXq7X4TaHrCUFBbJREAC7dU9CZYmKO-QUbw7ee7o580RGlD7W98Wjw"><i class="fa fa-globe" aria-hidden="true"></i>
+                      AIKTC OFFICIAL</a>
+                  </li>
+                  <li>
+                    <a href="https://www.facebook.com/aiktcofficial/"><i class="fa fa-facebook-official" aria-hidden="true"></i>Facebook</a>
+                  </li>
+                 
+                </ul>
+        
+              </div>
+              <!-- Grid column -->
+        
+              <!-- Grid column -->
+              <div class="col-md-3 mb-md-0 mb-3">
+        
+                <!-- Links -->
+                <h5 class="text-uppercase"><i class="fa fa-link" aria-hidden="true"></i>
+                  Links</h5>
+        
+                <ul class="list-unstyled">
+                  <li>
+                    <a href="mailto:aiktc.newpanvel@gmail.com"><i class="fa fa-envelope" aria-hidden="true"></i>aiktc.newpanvel@gmail.com</a>
+                  </li>
+                  <li>
+                    <a href="#!"><i class="fa fa-instagram" aria-hidden="true"></i>Instagram</a>
+                  </li>
+                 
+                </ul>
+        
+              </div>
+              <!-- Grid column -->
+        
+            </div>
+            <!-- Grid row -->
+        
+            
+          </div>
+          <!-- Footer Links -->
+          <div class="footer-copyright    text-center py-3">Managed by Sakib Arkate | Siddiqui Affan | Siddiqui Sufiyan
+           
+          </div>
+          <!-- Copyright -->
+        
+        </footer>
+          <!-- Copyright -->
+     
+        <!-- Footer --> 
+        
+ <script>
+   var loading = document.getElementById('loading');
+   function preloader(){
+     loading.style.display = 'none';
+   }
+ </script>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+  (function(){
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/5f44e4d5cc6a6a5947ae9780/default';
+  s1.charset='UTF-8';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
+  })();
+  </script>
+  <!--End of Tawk.to Script-->
+  
+                                                        <!-- Scripts -->
+  
+<!-- ------------ Scripts --------- -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://kit.fontawesome.com/95931f8504.js" crossorigin="anonymous"></script>
+<!-- ------------ Scripts Ends --------- -->
+</body>
+</html>
+
+
+?>
